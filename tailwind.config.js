fe3757134e84f8150c049a0c89b9a7d80c22e380
nodlag <nodlag@gmail.com>
2026-05-20 19:16:31 +0200

    Updated portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['./index.html'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                primary: '#3B82F6',
+            },
+            fontFamily: {
+                'm5x7': ['"m5x7"', 'monospace'],
+            },
+        },
+    },
+    plugins: [],
+};
